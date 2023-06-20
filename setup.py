59e5109c1d50b80e139d46ec26dfb856630cf3a3
Adrian <52701496+futzu@users.noreply.github.com>
2023-06-20 17:54:38 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+with open("version.py","r", encoding="utf-8") as latest:
+    version = latest.read().split("'")[1]
+
+setuptools.setup(
+    name="showcues",
+    version=version,
+    author="Adrian of Doom.",
+    author_email="spam@iodisco.com",
+    description="showcues displays HLS CUE-IN and CUE-OUT tags with wallclock time",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/futzu/showcues",
+    install_requires=[
+        'new_reader >= 0.1.7',
+        'm3ufu >= 0.0.69',
+        'threefive >= 2.3.97',
+    ],
+
+    scripts=['bin/showcues'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    python_requires=">=3.6",
+)
