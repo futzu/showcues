7add58c42764995659f0f459dee3f63edd3f8fcd
Adrian <52701496+futzu@users.noreply.github.com>
2023-09-11 16:05:08 +0000

    m3ufu v0.0.73

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     url="https://github.com/futzu/showcues",
     install_requires=[
         'new_reader >= 0.1.7',
-        'm3ufu >= 0.0.69',
+        'm3ufu >= 0.0.73',
         'threefive >= 2.4.9',
     ],
     py_modules=["showcues"],
